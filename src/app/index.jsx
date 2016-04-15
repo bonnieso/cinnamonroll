f0435122470e0bcefee5bcf92136d2f6b0087d62
Bonnie So <bonnie_so@apple.com>
2016-04-15 14:52:59 -0700

    adding frontend

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,17 +1,44 @@
 import React from 'react';
-import {render} from 'react-dom';
+import ReactDOM from 'react-dom';
+import RollList from './rollList.jsx';
 
-import AwesomeComponent from './AwesomeComponent.jsx';
+var RollApp = React.createClass({
+  getInitialState: function() {
+    return {items: [], text: ''};
+  },
+  onChange: function(e) {
+    this.setState({text: e.target.value});
+  },
+  handleSubmit: function(e) {
+    e.preventDefault();
+    var rollArray = [];
 
-class App extends React.Component {
-  render () {
+    var numberOfRolls = this.state.text.split('d')[0];
+    var numberOfSides = this.state.text.split('d')[1];
+
+    for(var i = 0; i < numberOfRolls; i++){
+      rollArray.push(getRandomInclusive(1, numberOfSides));
+    }
+    // var rolled = this.state.items.concat([{text: this.state.text, id: Math.random() }]);
+    // var nextText = '';
+    this.setState({items: rollArray});
+  },
+  render: function() {
     return (
       <div>
-        <p> Hello React!</p>
-        <AwesomeComponent />
+        <h3>Cinnamon Roll</h3>
+        <RollList items={this.state.items}/>
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.onChange} value={this.state.text} />
+          <button>Roll</button>
+        </form>
       </div>
     );
   }
+});
+
+function getRandomInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-render(<App/>, document.getElementById('app'));
+ReactDOM.render(<RollApp />, document.getElementById('app'));
---
+++ src/app/rollList.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+var RollList = React.createClass({
+  render: function() {
+    var createItem = function(item, index) {
+      return <li key = {index+1} >Dice {index+1} Result: {item}</li>;
+    };
+    return (
+      <ul>{this.props.items.map(createItem)}</ul>
+    );
+  }
+});
+
+module.exports = RollList;
