61a81e07e2dae0491a7c93696823ad650a1992e8
Bonnie So <bonnie_so@apple.com>
2016-06-10 17:48:37 -0700

    updating gulp file to include webpack

--- gulp/gulpfile.js
+++ gulp/gulpfile.js
@@ -1,8 +1,21 @@
 var gulp = require('gulp');
-
+var webpack = require('gulp-webpack');
 var plugins = require('gulp-load-plugins')();
+
+// gulp.task('default', function() {
+//   return gulp.src('../src/js/*.jsx')
+//     .pipe(webpack(require('../webpack.config.js')))
+//     .pipe(gulp.dest('../www/'));
+// });
+
 gulp.task('sass', require('./tasks/sass')(gulp, plugins));
 
-gulp.task('default',['sass'], function () {
+gulp.task('js', function() {
+  return gulp.src('../src/js/*.jsx')
+    .pipe(webpack(require('../webpack.config.js')))
+    .pipe(gulp.dest('../www/'));
+});
+
+gulp.task('default',['sass','js'], function () {
     gulp.watch('../src/sass/*.{sass,scss}', ['sass']);
 });
